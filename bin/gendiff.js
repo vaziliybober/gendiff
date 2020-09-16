3ea5a7021732d03aa0123221ead4a91566b22fd4
vaziliybober <vaziliybober@gmail.com>
2020-09-16 20:00:15 +0300

    add --format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,4 +7,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information');
 
+program
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
+
 program.parse(process.argv);
