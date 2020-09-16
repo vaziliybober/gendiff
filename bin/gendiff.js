da3d2007d009470c123cc601d8584a595452bfbc
vaziliybober <vaziliybober@gmail.com>
2020-09-16 21:07:49 +0300

    add basic program structure

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-
-const { program } = require('commander');
+import program from 'commander';
+import gendiff from '../src/index.js';
 
 program
   .version('0.0.1')
@@ -10,5 +10,8 @@ program
 program
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>');
+
+program
+  .action(gendiff);
 
 program.parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import fs from 'fs';
+import path from 'path';
+
+const gendiff = (filepath1, filepath2) => {
+  console.log('gendiff!!');
+};
+
+export default gendiff;
